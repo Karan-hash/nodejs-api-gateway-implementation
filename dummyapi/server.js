7fba0db7a2f8c2533826e901a517865e315f7b65
Karan-hash <karankaushal907@gmail.com>
2023-11-21 10:57:33 +0530

    Http Request sending Api gateway to dummy api done

--- dummyapi/server.js
+++ dummyapi/server.js
@@ -11,6 +11,10 @@ app.get('/dummyapi', (req, res, next) => {
     res.send("Hello From Dummy Api Server");
 })
 
+app.get('/dummybogusapi', (req, res, next) => {
+    res.send("Dummy Bogus Api Server called");
+})
+
 app.listen(PORT, () => {
     console.log("Dummy Server has started on port : ", PORT);
 })
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
-router.all('/:apiName', (req, res) => {
-    console.log(req.params.apiName);
-    res.send(req.params.apiName + '\n');
-})
+router.all("/:apiName", (req, res) => {
+  console.log(req.params.apiName);
+//   res.send(req.params.apiName + "\n");
+  axios
+    .get("http://localhost:8080/" + req.params.apiName)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data from the API:", error.message);
+      res.status(500).send("Internal Server Error");
+    });
+});
 module.exports = router;
